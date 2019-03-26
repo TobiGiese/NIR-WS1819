1fb5731d6ab3c2c9d9bfa56650bd5fe309a8472f
Tobias <tobias@giese-mann.de>
2019-03-26 14:49:25 +0100

    added chapter 3.2 in methodology

--- src/modelselection.R
+++ src/modelselection.R
@@ -27,6 +27,12 @@ nir.data = nirs.data[, 4:322]
 # wavelengths
 x=seq(1400, 2664, 4)
 
+plot(seq(1400, 2672, 4), nir.data[1,], type='l', col=1, ylim=c(0.3,0.7))
+lines(seq(1400, 2672, 4), nir.data[2,], type='l', col=2, ylim=c(0.3,0.7))
+lines(seq(1400, 2672, 4), nir.data[3,], type='l', col=3, ylim=c(0.3,0.7))
+lines(seq(1400, 2672, 4), nir.data[4,], type='l', col=4, ylim=c(0.3,0.7))
+lines(seq(1400, 2672, 4), nir.data[5,], type='l', col=5, ylim=c(0.3,0.7))
+
 # plot first deviation for first 6 records
 plot(x, getDiff(t(nir.data)[,1], 318), type='l', col=1)
 lines(x, getDiff(t(nir.data)[,2], 318), type='l', col=2)
@@ -41,33 +47,112 @@ M = getDiffMatrix(nir.data, 533, 318)
 Mavg = colMeans(M)
 plot(x, Mavg, type='l', col=1)
 # threshold 
-abline(h=0.0015, col=2)
+THRESHOLD = 0.0015
+abline(h=THRESHOLD, col=2)
 
 
 # filter dataset for colunms having an average value (1. deviation) above threshold
 df = data.frame(x, colnames(nirs.data[, 4:320]), Mavg)
 colnames(df) = c("x", "nm", "y")
-filteredAvg1stDev = subset(df, y >= 0.0015, select=c("x", "nm", "y"));
+filteredAvg1stDev = subset(df, y >= THRESHOLD, select=c("x", "nm", "y"));
 nrow(filteredAvg1stDev)
 plot(filteredAvg1stDev$x, filteredAvg1stDev$y, type='p', col=1)
 
 nirs_filtered.data = subset(nirs.data, select=c(c("SOC", "N", "pH"), as.character(filteredAvg1stDev[,"nm"])))
 
-# plot first deviation for first 6 records
-plot(x, getDiff(t(nirs_filtered.data)[,1], 318), type='l', col=1)
-lines(x, getDiff(t(nirs_filtered.data)[,2], 318), type='l', col=2)
-lines(x, getDiff(t(nirs_filtered.data)[,3], 318), type='l', col=3)
-lines(x, getDiff(t(nirs_filtered.data)[,4], 318), type='l', col=4)
-lines(x, getDiff(t(nirs_filtered.data)[,5], 318), type='l', col=5)
-lines(x, getDiff(t(nirs_filtered.data)[,6], 318), type='l', col=6)
-
-
 ### Mallows CP
 require(leaps)
 
-#subsets = regsubsets( N ~ nm1512+nm1520+nm1524+nm1528+nm1532+nm1536+nm1548+nm1552+nm2140+nm2144+nm2148+nm2152+nm2156+nm2160+nm2164+nm2168+nm2172+nm2176+nm2180+nm2184+nm2188+nm2192+nm2196+nm2200+nm2204+nm2208+nm2212+nm2216+nm2220+nm2224+nm2228+nm2232+nm2236+nm2240+nm2244+nm2248+nm2252+nm2256+nm2260+nm2264+nm2268+nm2272+nm2384+nm2388+nm2396+nm2400+nm2404+nm2408+nm2412+nm2416+nm2420+nm2424+nm2428+nm2432+nm2448+nm2452+nm2456+nm2464+nm2468+nm2472+nm2476+nm2480+nm2484+nm2488+nm2492+nm2496+nm2500+nm2504+nm2508+nm2512+nm2528+nm2532+nm2536+nm2540+nm2544+nm2548+nm2552+nm2556+nm2560+nm2564+nm2568+nm2572+nm2576+nm2580+nm2584+nm2588+nm2592+nm2596+nm2600+nm2604+nm2608+nm2636+nm2640+nm2652+nm2656+nm2660+nm2664, nirs_filtered.data, really.big=T, nvmax=97)
-subsets = regsubsets( N ~ nm2144+nm2148+nm2152+nm2156+nm2160+nm2164+nm2168+nm2172+nm2176+nm2180+nm2216+nm2220+nm2408+nm2412+nm2416+nm2424+nm2428+nm2476+nm2480+nm2504+nm2508+nm2512+nm2552+nm2556+nm2560+nm2564+nm2568+nm2572+nm2576+nm2580+nm2584+nm2588+nm2592+nm2596+nm2600+nm2656+nm2660+nm2664, nirs_filtered.data, really.big=F, nvmax=38)
-model.summary = summary(subsets)
-min.model.index = which.min(summary(subsets)$cp)
-model.summary
- 
+# Select model
+subsets = regsubsets( N ~ 1+nm2144+nm2148+nm2152+nm2156+nm2160+nm2164+nm2168+nm2172+nm2176+nm2180+nm2216+nm2220+nm2408+nm2412+nm2416+nm2424+nm2428+nm2476+nm2480+nm2504+nm2508+nm2512+nm2552+nm2556+nm2560+nm2564+nm2568+nm2572+nm2576+nm2580+nm2584+nm2588+nm2592+nm2596+nm2600+nm2656+nm2660+nm2664, nirs_filtered.data, really.big=F, nvmax=39, method="seqrep")
+optModelId = which.min(summary(subsets)$cp)
+coeffs = as.vector(coef(subsets, optModelId))
+
+
+
+# ##########################################################
+# ## SIMULATION
+# ##########################################################
+set.seed(13)
+getDesignmatrix = function(subsets, modelId, responsevar, data) {
+  X <- summary(subsets)$which
+  xvars <- dimnames(X)[[2]][-1]
+  id <- X[modelId,]
+  form <- reformulate(xvars[which(id[-1])], responsevar, id[1])
+  mod.nir <- lm(form, data)
+  designMatrix <- model.matrix(mod.nir)
+  
+  return(designMatrix)
+}
+
+getMeans = function(designMatrix, subsets, optModelId) {
+  coeffs = as.vector(coef(subsets, optModelId))
+  means = designMatrix %*% coeffs
+  
+  return(means)
+}
+
+getSDs = function(designMatrix, means, data.origin) {
+  residuals = as.vector(data.origin$N - means)
+  sd = sqrt(as.numeric(t(residuals) %*% residuals / (dim(data.origin)[1] - dim(designMatrix)[2])))
+  
+  return(sd)
+}
+
+# Generiert num Pseudodaten für Zielgröße N
+# num: Anzahl zu erzeugender Pseudodaten
+# TIMES: Anzahl wiederholungen
+simulate = function(subsets, modelId, data.origin, num, TIMES) {
+  data = data.frame(matrix(nrow = num, ncol=TIMES));
+  
+  designmatrix = getDesignmatrix(subsets, modelId, "N", data.origin)
+  means = getMeans(designmatrix, subsets, modelId)
+  sd = getSDs(designmatrix, means, data.origin)
+  
+  for (i in 1:TIMES) {
+    data[i] = rnorm(num, mean=means, sd = sd)  
+  }
+
+  dataAVG = rowMeans(data)
+  return(dataAVG)
+}
+
+# simulate 
+simdata = nirs_filtered.data;
+simdata$N = simulate(subsets, optModelId, nirs.data, nrow(nirs.data), 1000)
+plot(nirs.data[,4], nirs.data[,2], type="p", col=1, ylim=c(-0.1, 0.75))
+points(nirs.data[,4], simdata$N, type="p", col=2, ylim=c(-0.1, 0.75))
+
+simsets533 = regsubsets( N ~ 1+nm2144+nm2148+nm2152+nm2156+nm2160+nm2164+nm2168+nm2172+nm2176+nm2180+nm2216+nm2220+nm2408+nm2412+nm2416+nm2424+nm2428+nm2476+nm2480+nm2504+nm2508+nm2512+nm2552+nm2556+nm2560+nm2564+nm2568+nm2572+nm2576+nm2580+nm2584+nm2588+nm2592+nm2596+nm2600+nm2656+nm2660+nm2664, simdata, really.big=F, nvmax=39)
+optModelId533 = which.min(summary(simsets533)$cp)
+print(paste("========= Num Rows: 533 ========="))
+print(paste("Optimal Model (ID): ", optModelId533))
+print("CP Value")
+print(summary(simsets533)$cp)
+
+# Shrink designmatrix
+getDataSubset = function(data.origin, numRows) {
+  if (numRows < nrow(data.origin)) {
+    return(nirs.data[sample(nrow(data.origin), numRows), ])
+  }
+  
+  return(nirs.data)
+}
+
+# rum simulation and second model selection for a subset of nirs.data with increasing size
+# TODO: repeat simulation and model selection x times
+for (numRows in seq(50, 500, 50)) {
+  print(paste("========= Num Rows: ", numRows, " ========="))
+  nirs.data100 = getDataSubset(nirs.data, numRows)
+  simdata100 = subset(nirs.data100, select=c(c("SOC", "N", "pH"), as.character(filteredAvg1stDev[,"nm"])))
+  simdata100$N = simulate(subsets, optModelId, nirs.data100, numRows, 1000)
+  plot(nirs.data[,4], nirs.data[,2], type="p", col=1, ylim=c(-0.1, 0.75))
+  points(nirs.data100[,4], simdata100$N, type="p", col=2, ylim=c(-0.1, 0.75))
+  
+  simsets100 = regsubsets( N ~ 1+nm2144+nm2148+nm2152+nm2156+nm2160+nm2164+nm2168+nm2172+nm2176+nm2180+nm2216+nm2220+nm2408+nm2412+nm2416+nm2424+nm2428+nm2476+nm2480+nm2504+nm2508+nm2512+nm2552+nm2556+nm2560+nm2564+nm2568+nm2572+nm2576+nm2580+nm2584+nm2588+nm2592+nm2596+nm2600+nm2656+nm2660+nm2664, simdata100, really.big=F, nvmax=39)
+  optModelId100 = which.min(summary(simsets100)$cp)
+  print(paste("Optimal Model (ID): ", optModelId100))
+  print("CP Value")
+  print(summary(simsets100)$cp)
+}
+
